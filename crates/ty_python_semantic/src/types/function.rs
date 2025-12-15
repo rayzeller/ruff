a108f3003a32b40e161f040d4edeab5bdd790e90
Alex Waygood <alex.waygood@gmail.com>
2025-12-15 20:01:51 +0000

    wip

--- crates/ty_python_semantic/src/types/function.rs
+++ crates/ty_python_semantic/src/types/function.rs
@@ -394,12 +394,12 @@ impl<'db> OverloadLiteral<'db> {
             return None;
         }
 
-        // These can both happen in edge cases involving not-yet-complete function definitions
-        // (invalid syntax).
+        // These can both happen in edge cases where a definition created with a `def`
+        // statement shadows a non-`def` symbol with the same name.
         if previous_overload.name(db) != self.name(db) {
             return None;
         }
-        if previous_overload.body_scope(db).file(db) != self.file(db) {
+        if previous_overload.body_scope(db) != self.body_scope(db) {
             return None;
         }
 
